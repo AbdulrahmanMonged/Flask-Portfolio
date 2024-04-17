ef257b311defe0c604c1f78b4b1bdf11b0c5c856
AbdulrahmanMonged <abdoomonged231@gmail.com>
2024-04-17 04:09:37 +0200

    init

--- api/app.py
+++ api/app.py
@@ -3,24 +3,29 @@ from flask import request
 from flask_restful import Resource
 import resend
 
+
 @app.route("/")
 def home():
     return "Hello World!"
+
 
 class SendEmail(Resource):
     def post(self):
         try:
             data = request.get_json()
             params = {
-            "from": "Your own Portfolio <onboarding@resend.dev>",
-            "to": email,
-            "subject": data['subject'],
-            "html": "<p><b>From: {0}</b></p><h6>Message: </h6><p>{1}</p>".format(data['email'], data['message']),
+                "from": "Your own Portfolio <onboarding@resend.dev>",
+                "to": email,
+                "subject": data["subject"],
+                "html": "<p><b>From: {0}</b></p><h6>Message: </h6><p>{1}</p>".format(
+                    data["email"], data["message"]
+                ),
             }
             resend.Emails.send(params)
             return "Sent Successfully!", 201
         except Exception as e:
-            return "Error occured while validation Data", 400 
+            return "Error occured while validation Data", 400
+
 
 api.add_resource(SendEmail, "/email")
 
