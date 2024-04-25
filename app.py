761b0c133fcd1b88e7833b230f12a9bf8dbe406c
AbdulrahmanMonged <abdoomonged231@gmail.com>
2024-04-25 11:25:47 +0200

    Test2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import request
 from flask_restful import Resource
 import resend
 
+
 @app.route("/")
 def home():
     return "Hello World!"
@@ -16,16 +17,53 @@ class SendEmail(Resource):
                 "from": "Your own Portfolio <onboarding@resend.dev>",
                 "to": email,
                 "subject": data["subject"],
-                "html": """<p><b>From: {0}</b></p><h6>Message: </h6><p>{1}</p>""".format(
-                    data["email"], data["message"]
+                "html": """<!DOCTYPE html>
+                <html>
+                <head>
+                <style>
+    body {
+      font-family: Arial, sans-serif;
+    }
+    .container {
+      width: 80%;
+      margin: auto;
+      padding: 20px;
+      border: 1px solid #ddd;
+      border-radius: 5px;
+      background-color: #f9f9f9;
+    }
+    .header {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+    .content {
+      margin-bottom: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>Hello there!</h1>
+    </div>
+    <div class="content">
+      <p>Email: {0}</p>
+      <p>Subject: {2}</p>
+      <p>Message:</p>
+      <p style="white-space: pre-wrap;">{1}</p>
+    </div>
+  </div>
+</body>
+</html>""".format(
+                    data["email"]["value"],
+                    data["message"]["value"],
+                    data["subject"]["value"],
                 ),
             }
             resend.Emails.send(params)
             return "Sent Successfully!", 201
         except Exception as e:
-            print(e)
             return "Error occured while validation Data", 400
 
 
 api.add_resource(SendEmail, "/email")
-
