a1df2dc7a8a637cd523c0d5cbf91ffc692cef44f
AbdulrahmanMonged <abdoomonged231@gmail.com>
2024-04-25 11:31:50 +0200

    it should prine errors noew

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import request
 from flask_restful import Resource
 import resend
 
-
 @app.route("/")
 def home():
     return "Hello World!"
@@ -17,47 +16,8 @@ class SendEmail(Resource):
                 "from": "Your own Portfolio <onboarding@resend.dev>",
                 "to": email,
                 "subject": data["subject"],
-                "html": """<!DOCTYPE html>
-                <html>
-                <head>
-                <style>
-    body {
-      font-family: Arial, sans-serif;
-    }
-    .container {
-      width: 80%;
-      margin: auto;
-      padding: 20px;
-      border: 1px solid #ddd;
-      border-radius: 5px;
-      background-color: #f9f9f9;
-    }
-    .header {
-      text-align: center;
-      margin-bottom: 20px;
-    }
-    .content {
-      margin-bottom: 20px;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <div class="header">
-      <h1>Hello there!</h1>
-    </div>
-    <div class="content">
-      <p>Email: {0}</p>
-      <p>Subject: {2}</p>
-      <p>Message:</p>
-      <p style="white-space: pre-wrap;">{1}</p>
-    </div>
-  </div>
-</body>
-</html>""".format(
-                    data["email"]["value"],
-                    data["message"]["value"],
-                    data["subject"]["value"],
+                "html": "<p>Email: {0}</p><p>Subject: {2}</p><p>Message:</p><p style='white-space: pre-wrap;'>{1}</p>".format(
+                    data["email"]["value"], data["message"]["value"], data["subject"]["value"]
                 ),
             }
             resend.Emails.send(params)
@@ -68,3 +28,4 @@ class SendEmail(Resource):
 
 
 api.add_resource(SendEmail, "/email")
+
